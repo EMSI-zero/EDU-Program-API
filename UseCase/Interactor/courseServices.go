ef2b08669e1041fc98d8d1ec520898ae9360ed65
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-06-05 19:21:31 +0430

    Define FindRequirements
    -Defines course service FindRequirements

--- UseCase/Interactor/courseServices.go
+++ UseCase/Interactor/courseServices.go
@@ -15,6 +15,17 @@ func (ci * courseInteractor) GetCourses (courses []*course.Course) ([]*course.Co
 
 func (ci * courseInteractor) FindCoursesByTitle (courses []*course.Course , s []string) (  []*course.Course, error)
 func (ci * courseInteractor) FindCoursesByID (  courses []*course.Course , ID []uint) ([]*course.Course , error)
-func (ci * courseInteractor) FindRequirements (courses []*course.Course , ID uint ) ([]*course.Course, error)
+
+func (ci * courseInteractor) FindRequirements (courses []*course.Course , ID uint ) ([]*course.Course, error){
+	cs , err := ci.CourseRepository.findPreReqs(courses)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ci.CoursePresenter.ResponseCourses(cs) , nil
+}
+
+
 func (ci * courseInteractor) CreateCourse (c []*course.Course) (error)
 func (ci * courseInteractor) DeleteCourse (c []*course.Course) (error)
