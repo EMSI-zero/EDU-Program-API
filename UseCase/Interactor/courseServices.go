f6b71d2e3fbffb15fd1cf2f1a3b22787fd31a344
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-06-05 19:17:35 +0430

    Define GetCourses
    -Defines course service GetCourses

--- UseCase/Interactor/courseServices.go
+++ UseCase/Interactor/courseServices.go
@@ -3,7 +3,16 @@ package interactor
 import "Basic-Edu-Program/Domain/models/course"
 
 
-func (ci * courseInteractor) GetCourses (courses []*course.Course) ([]*course.Course, error)
+func (ci * courseInteractor) GetCourses (courses []*course.Course) ([]*course.Course, error){
+	cs, err := ci.CourseRepository.findAll(courses)
+	if err != nil {
+		return nil, err
+	}
+
+	return ci.CoursePresenter.ResponseCourses(cs), nil
+}
+
+
 func (ci * courseInteractor) FindCoursesByTitle (courses []*course.Course , s []string) (  []*course.Course, error)
 func (ci * courseInteractor) FindCoursesByID (  courses []*course.Course , ID []uint) ([]*course.Course , error)
 func (ci * courseInteractor) FindRequirements (courses []*course.Course , ID uint ) ([]*course.Course, error)
