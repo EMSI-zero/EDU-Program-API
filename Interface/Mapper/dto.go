fe5cc319c4f1fc1839326779ae27e89fb7ccd03e
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-07-06 23:50:16 +0430

    Add course DTO
    -Adds course dto
    -Implements presenter methods

--- Interface/Mapper/dto.go
+++ Interface/Mapper/dto.go
@@ -4,3 +4,24 @@ import (
 	"Basic-Edu-Program/Domain/model"
 	"Basic-Edu-Program/UseCase/presenter"
 )
+
+type courseDto struct{
+
+}
+
+type coursePresenter struct{}
+
+
+func NewCoursePresenter() presenter.CoursePresenter{
+	return &coursePresenter{}
+}
+
+func (cd *coursePresenter) ResponsePreReqs(c []* model.Course) ([]*model.Course , error){
+	return nil,nil
+}
+func (cd *coursePresenter) ResponseOperation(error) (string){
+	return ""
+}
+func (cd *coursePresenter) ResponseCourses(c []*model.Course) ([]*model.Course){
+	return nil
+}
